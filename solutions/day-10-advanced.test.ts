b2663c7b147c9934e809db866536bb19217e6602
nizo <martin.niznansky@gmail.com>
2023-12-12 12:09:37 +0100

    Day 10

--- solutions/day-10-advanced.test.ts
+++ solutions/day-10-advanced.test.ts
@@ -1,4 +1,5 @@
 import { test, expect, describe } from "bun:test";
+import { collapseTextChangeRangesAcrossMultipleVersions } from "typescript";
 
 type Direction = "up" | "right" | "down" | "left";
 type Path = {
@@ -7,33 +8,32 @@ type Path = {
 	};
 };
 
+type vertex = [number, number];
 let map: string[][] = [];
 
-const computeInnerArea = (node: Path): number => {
-	let nodesWithinPath = 0;
-	const nodesInside: Path = {};
+const getInnerArea = (vertices: vertex[]): number => {
+	let area = 0;
 
-	for (let y = 0; y < map.length; y++) {
-		let isWithinPath = false;
-		for (let x = 0; x < map[y].length; x++) {
-			const currentItem = map[y][x];
-			if (node[y] && node[y][x]) {
-				if (currentItem === "|") isWithinPath = !isWithinPath;
-				else if (currentItem === "7" && map[y + 1][x] === "L") isWithinPath = !isWithinPath;
-				else if (currentItem === "J" && map[y - 1][x] === "F") isWithinPath = !isWithinPath;
-			}
+	// https://en.wikipedia.org/wiki/Shoelace_formula
+	const length = vertices.length;
+	let sum1 = 0;
+	let sum2 = 0;
 
-			if (currentItem == "." && isWithinPath) {
-				console.log(y, x);
-				nodesWithinPath++;
-			}
-		}
+	for (let i = 0; i < length - 1; i++) {
+		sum1 = sum1 + vertices[i][0] * vertices[i + 1][1];
+		sum2 = sum2 + vertices[i][1] * vertices[i + 1][0];
 	}
 
-	return nodesWithinPath;
+	sum1 = sum1 + vertices[length - 1][0] * vertices[0][1];
+	sum2 = sum2 + vertices[0][0] * vertices[length - 1][1];
+
+	area = Math.abs(sum1 - sum2) / 2;
+
+	// https://en.wikipedia.org/wiki/Pick%27s_theorem
+	return area + 1 - vertices.length / 2;
 };
 
-const getNodes = (x: number, y: number, direction: Direction, nodes: Path): Path | null => {
+const getNodes = (x: number, y: number, direction: Direction, nodes: Path, vertices: vertex[]): vertex[] | null => {
 	// error cases
 	if (x < 0) return null;
 	if (y < 0) return null;
@@ -44,7 +44,6 @@ const getNodes = (x: number, y: number, direction: Direction, nodes: Path): Path
 	if (currentSign === ".") return null;
 
 	// if already visited this path
-
 	if (nodes[y] && nodes[y][x]) {
 		console.log("loop, early exit");
 		return null;
@@ -53,40 +52,44 @@ const getNodes = (x: number, y: number, direction: Direction, nodes: Path): Path
 	// winning node
 	if (map[y][x] === "S") {
 		nodes[y][x] = 1;
-		return nodes;
+		vertices.push([x, y]);
+		return vertices;
 	}
 
 	if (!nodes[y]) nodes[y] = {};
-	nodes[y][x] = 1;
+	{
+		nodes[y][x] = 1;
+		vertices.push([x, y]);
+	}
 
 	if (currentSign === "|") {
-		if (direction === "up") return getNodes(x, y - 1, "up", nodes);
-		if (direction === "down") return getNodes(x, y + 1, "down", nodes);
+		if (direction === "up") return getNodes(x, y - 1, "up", nodes, vertices);
+		if (direction === "down") return getNodes(x, y + 1, "down", nodes, vertices);
 	}
 
 	if (currentSign === "-") {
-		if (direction === "right") return getNodes(x + 1, y, "right", nodes);
-		if (direction === "left") return getNodes(x - 1, y, "left", nodes);
+		if (direction === "right") return getNodes(x + 1, y, "right", nodes, vertices);
+		if (direction === "left") return getNodes(x - 1, y, "left", nodes, vertices);
 	}
 
 	if (currentSign === "L") {
-		if (direction === "left") return getNodes(x, y - 1, "up", nodes);
-		if (direction === "down") return getNodes(x + 1, y, "right", nodes);
+		if (direction === "left") return getNodes(x, y - 1, "up", nodes, vertices);
+		if (direction === "down") return getNodes(x + 1, y, "right", nodes, vertices);
 	}
 
 	if (currentSign === "J") {
-		if (direction === "right") return getNodes(x, y - 1, "up", nodes);
-		if (direction === "down") return getNodes(x - 1, y, "left", nodes);
+		if (direction === "right") return getNodes(x, y - 1, "up", nodes, vertices);
+		if (direction === "down") return getNodes(x - 1, y, "left", nodes, vertices);
 	}
 
 	if (currentSign === "7") {
-		if (direction === "up") return getNodes(x - 1, y, "left", nodes);
-		if (direction === "right") return getNodes(x, y + 1, "down", nodes);
+		if (direction === "up") return getNodes(x - 1, y, "left", nodes, vertices);
+		if (direction === "right") return getNodes(x, y + 1, "down", nodes, vertices);
 	}
 
 	if (currentSign === "F") {
-		if (direction === "up") return getNodes(x + 1, y, "right", nodes);
-		if (direction === "left") return getNodes(x, y + 1, "down", nodes);
+		if (direction === "up") return getNodes(x + 1, y, "right", nodes, vertices);
+		if (direction === "left") return getNodes(x, y + 1, "down", nodes, vertices);
 	}
 
 	return null;
@@ -109,19 +112,27 @@ async function solution(file: string): Promise<number> {
 		position = [i, startPosition];
 	}
 
-	let result: Path | null = null;
+	let result: vertex[] | null = null;
 
-	result = getNodes(position[1], position[0] + 1, "down", {});
-	if (result !== null) return computeInnerArea(result);
+	result = getNodes(position[1], position[0] + 1, "down", {}, []);
+	if (result !== null) {
+		return getInnerArea(result);
+	}
 
-	result = getNodes(position[1], position[0] - 1, "up", {});
-	if (result !== null) return computeInnerArea(result);
+	result = getNodes(position[1], position[0] - 1, "up", {}, []);
+	if (result !== null) {
+		return getInnerArea(result);
+	}
 
-	result = getNodes(position[1] - 1, position[0], "left", {});
-	if (result !== null) return computeInnerArea(result);
+	result = getNodes(position[1] - 1, position[0], "left", {}, []);
+	if (result !== null) {
+		return getInnerArea(result);
+	}
 
-	result = getNodes(position[1] + 1, position[0], "right", {});
-	if (result !== null) return computeInnerArea(result);
+	result = getNodes(position[1] + 1, position[0], "right", {}, []);
+	if (result !== null) {
+		return getInnerArea(result);
+	}
 
 	throw "no such cycle exists";
 }
@@ -134,18 +145,22 @@ const testCases: Array<{
 		input: "./input/day-10-advanced-01a.txt",
 		output: 4,
 	},
-	// {
-	// 	input: "./input/day-10-advanced-01b.txt",
-	// 	output: 4,
-	// },
-	// {
-	// 	input: "./input/day-10-advanced-02.txt",
-	// 	output: 8,
-	// },
-	// {
-	// 	input: "./input/day-10-advanced-03.txt",
-	// 	output: 6690,
-	// },
+	{
+		input: "./input/day-10-advanced-01b.txt",
+		output: 4,
+	},
+	{
+		input: "./input/day-10-advanced-02.txt",
+		output: 8,
+	},
+	{
+		input: "./input/day-10-advanced-03.txt",
+		output: 10,
+	},
+	{
+		input: "./input/day-10-advanced-04.txt",
+		output: 525,
+	},
 ];
 
 describe("advent-of-code-2023 #10", () => {
