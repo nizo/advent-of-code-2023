a1e83003931a890c2c165d34cd66b1bcf6059d96
nizo <martin.niznansky@gmail.com>
2023-12-14 09:28:25 +0100

    day 12, part I

--- solutions/day-12-base.test.ts
+++ solutions/day-12-base.test.ts
@@ -1,19 +1,14 @@
 import { test, expect, describe } from "bun:test";
 
-const getArrangementsCount = (line: string, groups: number[]): number => {
-	if (line === "") return 0;
+const getVariations = (line: string): string => {
+	if (line === "") return "";
 
-	let index = 0;
+	const index = line.indexOf("?");
+	if (index == -1) return line;
 
-	while (index < line.length - 1) {
-		if (line[index] === ".") index++;
-	}
-
-	if (line.substring(0, groups[0]).includes(".")) {
-		return 0;
-	}
-
-	//return getArrangementsCount();
+	const variationA = line.substring(0, index) + "." + line.substring(index + 1);
+	const variationB = line.substring(0, index) + "#" + line.substring(index + 1);
+	return getVariations(variationA) + "," + getVariations(variationB);
 };
 
 async function solution(file: string): Promise<number> {
@@ -21,23 +16,37 @@ async function solution(file: string): Promise<number> {
 	const text = await inputFile.text();
 	const lines = text.split("\n");
 
-	let arrangementsCount = 0;
+	let variations = "";
+	let validVariationsCount = 0;
 
 	for (let line of lines) {
 		const [input, groups] = line.split(" ");
 
 		if (groups === undefined) {
-			arrangementsCount++;
 			continue;
 		}
 
-		arrangementsCount += getArrangementsCount(
-			input,
-			groups.split(",").map((item) => parseInt(item))
-		);
+		variations = getVariations(input);
+
+		let variationsArray = variations.split(",");
+		const groupsArray = groups.split(",").map((item) => parseInt(item));
+
+		for (let variation of variationsArray) {
+			const data = variation.split(".").filter((item) => item !== "");
+			if (data.length !== groupsArray.length) continue;
+
+			let valid = true;
+			for (let i = 0; i < groupsArray.length; i++) {
+				if (groupsArray[i] !== data[i].length) {
+					valid = false;
+					break;
+				}
+			}
+			if (valid) validVariationsCount++;
+		}
 	}
 
-	return 0;
+	return validVariationsCount;
 }
 
 const testCases: Array<{
@@ -46,12 +55,12 @@ const testCases: Array<{
 }> = [
 	{
 		input: "./input/day-12-base-01.txt",
-		output: 374,
+		output: 10,
 	},
-	// {
-	// 	input: "./input/day-12-base-02.txt",
-	// 	output: 9795148,
-	// },
+	{
+		input: "./input/day-12-base-02.txt",
+		output: 7718,
+	},
 ];
 
 describe("advent-of-code-2023 #12", () => {
